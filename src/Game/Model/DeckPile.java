06a69cfa4058dbd100c08f0f49a5ee4c2bc64ed7
isling98 <s174879@student.dtu.dk>
2020-04-01 12:20:53 +0200

    start på Deck og Discard
    .

--- src/Game/Model/DeckPile.java
+++ src/Game/Model/DeckPile.java
@@ -1,4 +1,29 @@
 package Game.Model;
 
+import javax.sound.sampled.Line;
+import java.util.LinkedList;
+
 public class DeckPile extends CardPile {
+
+        LinkedList<Card> deckPopCard = new LinkedList<>();
+
+        public LinkedList fjernTop() {
+            /*
+            Nedenstående kan nok laves lettere/pænere med et for-each loop
+             */
+            for (int i = 0; i < 3; i++) {
+                deckPopCard.add(popCard());
+                if (LinkedCards.size() == 2){
+                    deckPopCard.add(popCard());
+                    deckPopCard.add(popCard());
+                }else if (LinkedCards.size() == 1){
+                    deckPopCard.add(popCard());
+                }else if (LinkedCards.isEmpty()){
+                    LinkedCards.addAll(deckPopCard);
+                    deckPopCard.removeAll(LinkedCards);
+                }
+            }
+            return deckPopCard;
+        }
+
 }
--- src/Game/Model/DiscardPile.java
+++ src/Game/Model/DiscardPile.java
@@ -1,4 +1,16 @@
 package Game.Model;
 
+import java.util.LinkedList;
+
 public class DiscardPile extends CardPile {
+
+    void addCard(LinkedList<Card> list){
+        LinkedCards.addAll(list);
+
+        /*
+        Her skal også køres så det øverste kort er 'synligt', og så skal der tjekkes, hvad der skal ske
+        med kortet. Hvis det passer på en suit bunke, skal det flyttes derover, hvis det passer nede i en af game-
+        bunkerne skal det derned og ellers skal det bare blive liggende.
+         */
+    }
 }
